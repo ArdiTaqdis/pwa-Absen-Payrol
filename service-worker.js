d0de97e42b3887129460dc9e2f3f9e71e7fff54b
ArdiTaqdis <kejajarwsb84@gmail.com>
2025-07-24 14:20:46 +0700

    gaji terbaru diatas

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'absen-sds-v2.4'; // ✅ Ganti versi saat ada update
+const CACHE_NAME = 'absen-sds-v2.5'; // ✅ Ganti versi saat ada update
 const FILES_TO_CACHE = [
   '/',
   'index.html',
