282aa931f4dbfbab076eac872624fbaf8cefab4d
ArdiTaqdis <kejajarwsb84@gmail.com>
2025-08-02 11:13:58 +0700

    v12

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'absen-sds-v2.10'; // ✅ Ganti versi saat ada update
+const CACHE_NAME = 'absen-sds-v2.12'; // ✅ Ganti versi saat ada update
 const FILES_TO_CACHE = [
   '/',
   'index.html',
